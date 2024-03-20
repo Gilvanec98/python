50bb271e51b0a01c683eb07195e8d1bb8c171a78
Gilvanec98 <164096888+Gilvanec98@users.noreply.github.com>
2024-03-19 22:03:44 -0300

    Add files via upload

---
+++ exercicioc7.py
@@ -0,0 +1,16 @@
+idade = int(input('Digite a idade '))
+
+
+if (idade >= 65):
+
+        print('Idosa')
+
+else:
+
+    if (idade >= 18):
+        print('Adulta')
+
+    else:
+
+    
+        print('Menor de Idade')
